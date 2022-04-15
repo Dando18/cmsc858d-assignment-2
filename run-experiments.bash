98f1dde88771e1a5cb97647a4a656de17d514aa0
Daniel Nichols <dando18studios@gmail.com>
2022-04-15 12:03:34 -0400

    experiment setup script

---
+++ run-experiments.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Runs a list of experiments to reproduce reported results.
+# author: Daniel Nichols
+# date: April 2022
+
+# settings
+BUILD_EXEC="./bin/buildsa"
+QUERY_EXEC="./bin/querysa"
+
+# rebuild binaries
+make clean
+make USE_OPENMP=0
+
+# run tests
+# TODO
+
+
+# run experiments
+# TODO
+
+
+# plot results
+# TODO
