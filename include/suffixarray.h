76b113da456461ff59c96cf1099dfc233c5b9ed7
Daniel Nichols <dando18studios@gmail.com>
2022-04-26 10:07:24 -0400

    support simpleaccelerant querying

--- include/suffixarray.h
+++ include/suffixarray.h
@@ -11,8 +11,10 @@
 #include <algorithm>    // find, find_if_not
 #include <array>        // array
 #include <exception>    // ios_base::failure
+#include <execution>    // execution::par_unseq
 #include <fstream>      // ifstream
 #include <ostream>      // ostream
+#include <random>       // 
 #include <string>       // string
 #include <unordered_map>// unordered_map
 
@@ -29,9 +31,11 @@ namespace suffixarray {
 class SuffixArray {
     constexpr static uint32_t FILE_MAGIC = 0xabeefdad;
     typedef std::unordered_map<std::string, std::pair<int32_t, int32_t>> PrefixTable;
-    enum QueryMode { Naive, SimpleAccelerant };
 
 public:
+    enum QueryMode { Naive, SimpleAccelerant };
+    struct Query { std::string title, query; std::vector<int32_t> result; };
+    
 
     /**
      * @brief Creates and returns a SuffixArray from the specified path.
@@ -42,7 +46,7 @@ public:
      * @param prefixTableLength build a prefix table for all k size prefixes
      * @return SuffixArray A SuffixArray object constructed on this path
      */
-    static SuffixArray fromFile(std::string const& path, int32_t prefixTableLength=0) {
+    static SuffixArray fromFASTAFile(std::string const& path, int32_t prefixTableLength=0) {
         std::ifstream inputFile(path);
         if (!inputFile) {
             throw std::ios_base::failure("File \"" + path + "\" could not be opened.");
@@ -59,6 +63,18 @@ public:
     }
 
     /**
+     * @brief Load a SuffixArray from a previously save SuffixArray
+     * 
+     * @param path file path to saved SuffixArray
+     * @return SuffixArray the suffix array from the specified file
+     */
+    static SuffixArray fromSave(std::string const& path) {
+        SuffixArray sa("");
+        sa.load(path);
+        return sa;
+    }
+
+    /**
      * @brief Creates and returns a SuffixArray from the given string.
      * @see fromFile
      * 
@@ -70,12 +86,96 @@ public:
         return SuffixArray(values, prefixTableLength);
     }
 
-    void query(QueryMode mode = QueryMode::Naive) const {
-        if (mode == QueryMode::SimpleAccelerant) {
+    /**
+     * @brief Query the SuffixArray for the specific Query. Query object specifies the query string
+     * and will hold the result of the query.
+     * If a prefix table exists, then it will be used to accelerate the query.
+     * 
+     * @param q Query object. Also receives the query results.
+     * @param mode Whether to use Naive or SimpleAccelerant method.
+     */
+    void query(Query &q, QueryMode mode = QueryMode::Naive) const {
+        auto searchStart = std::begin(suffixes_);
+        auto searchEnd = std::end(suffixes_);
 
-        } else {
-
+        /* narrow range with prefix table if possible */
+        if (!prefixTable_.empty() && q.query.size() >= prefixTableSize_) {
+            const auto subQuery = q.query.substr(prefixTableSize_);
+            if (prefixTable_.contains(subQuery)) {
+                searchStart = std::next(std::begin(suffixes_), prefixTable_.at(subQuery).first);
+                searchEnd = std::next(std::begin(suffixes_), prefixTable_.at(subQuery).second);
+            }
         }
+
+        /*  unfortunately std::equal_ranges won't work here because the search object has to 
+            have the same type as the container; i.e. you can't search on a well-ordered predicate, but have to 
+            search for a value; boost::transform_iterator's would make this a lot cleaner */
+
+        /* lower_bound */
+        auto [lower, upper] = std::tuple(searchStart, searchEnd);
+        auto range = std::distance(lower, upper);
+        while (range > 0) {
+            auto iter = lower;
+            auto step = range / 2;
+            std::advance(iter, step);
+
+            size_t searchOffset = 0;
+            if (mode == SimpleAccelerant) {
+                const auto lowerLCP = utilities::LCPLength(std::cbegin(q.query), std::cend(q.query), std::next(std::begin(data_), *lower));
+                const auto upperLCP = utilities::LCPLength(std::cbegin(q.query), std::cend(q.query), std::next(std::begin(data_), *iter));
+                searchOffset = std::min(lowerLCP, upperLCP);
+            }
+            const auto searchLength = q.query.size() - searchOffset;            
+
+            if (data_.compare((*iter)+searchOffset, searchLength, q.query, searchOffset, searchLength) < 0) {  /* true if data_[] < queryStr */
+                lower = ++iter;
+                range -= step + 1;
+            } else {
+                range = step;
+            }
+        }
+        const auto lowerIndex = lower;
+
+        /* upper_bound */
+        std::tie(lower, upper) = std::tuple(searchStart, searchEnd);
+        range = std::distance(lower, upper);
+        while (range > 0) {
+            auto iter = lower;
+            auto step = range / 2;
+            std::advance(iter, step);
+
+            size_t searchOffset = 0;
+            if (mode == SimpleAccelerant) {
+                const auto lowerLCP = utilities::LCPLength(std::cbegin(q.query), std::cend(q.query), std::next(std::begin(data_), *lower));
+                const auto upperLCP = utilities::LCPLength(std::cbegin(q.query), std::cend(q.query), std::next(std::begin(data_), *iter));
+                searchOffset = std::min(lowerLCP, upperLCP);
+            }
+            const auto searchLength = q.query.size() - searchOffset; 
+
+            if (q.query.compare(searchOffset, searchLength, data_, (*iter)+searchOffset, searchLength) >= 0) {
+                lower = ++iter;
+                range -= step + 1;
+            } else {
+                range = step;
+            }
+        }
+        const auto upperIndex = lower;
+
+        q.result = std::vector<int32_t>(lowerIndex, upperIndex);
+    }
+
+    /**
+     * @brief Perform a set of queries. If enabled, will do them in parallel.
+     * @see query
+     * 
+     * @tparam Iterator iterator which returns SuffixArray::Query objects.
+     * @param begin Start of query range.
+     * @param end End of query range.
+     * @param mode Querying method.
+     */
+    template <typename Iterator>
+    void queries(Iterator begin, Iterator end, QueryMode mode = QueryMode::Naive) {
+        std::for_each(utilities::executionPolicy, begin, end, [this, mode](auto &q) { this->query(q, mode); });
     }
 
     /**
@@ -87,10 +187,34 @@ public:
         return data_;
     }
 
+    /**
+     * @brief Returns the underlying suffix array.
+     * 
+     * @return std::vector<int32_t> const& Array of indices into string for each suffix.
+     */
+    std::vector<int32_t> const& suffixes() const noexcept {
+        return suffixes_;
+    }
+
+    size_t getPrefixTableSize() const noexcept {
+        return prefixTableSize_;
+    }
+
+    /**
+     * @brief Returns the duration it took to build SuffixArray.
+     * 
+     * @return double Duration to build SuffixArray
+     */
     double getSuffixArrayBuildTime() const noexcept {
         return suffixArrayBuildTime_;
     }
 
+    /**
+     * @brief Returns the duration it took to build PrefixTable. Returns 0 if no 
+     * prefix table was built.
+     * 
+     * @return double PrefixTable build duration
+     */
     double getPrefixTableBuildTime() const noexcept {
         return prefixTableBuildTime_;
     }
@@ -222,10 +346,21 @@ private:
      * @param prefixTableSize build a `k` prefix table. 0 for none.
      */
     SuffixArray(std::string const& data, size_t prefixTableSize=0)
-        : data_(data+"$"), prefixTableSize_(prefixTableSize), suffixes_(data_.size()) {
+        : data_(data), prefixTableSize_(prefixTableSize), suffixes_(data_.size()+1) {
+
+        std::random_device dev;
+        std::mt19937 rng(dev());
+        std::uniform_int_distribution<uint8_t> dist(0,3);
+        const char ALPHABET[4] = {'A', 'T', 'G', 'C'};
+        std::for_each(utilities::executionPolicy, std::begin(data_), std::end(data_), [&dist,&rng,&ALPHABET](auto &c) {
+            c = std::toupper(c);
+            if (c != 'A' && c != 'T' && c != 'G' && c != 'C') {
+                c = ALPHABET[dist(rng)];
+            }
+        });
+        data_.push_back('$');
 
         utilities::Timer timer;
-
         timer.start();
         this->buildSuffixArray();
         timer.stop();
@@ -340,10 +475,6 @@ private:
         }
     }
 
-
-    void queryNaive() {
-        
-    }
 };
 
 }   // namespace suffixarray
