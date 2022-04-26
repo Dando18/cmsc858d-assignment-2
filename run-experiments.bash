272bf4c4cd3b8382416d8644b6e8982bcab2c91f
Daniel Nichols <dando18studios@gmail.com>
2022-04-26 10:06:51 -0400

    update to experiments

--- run-experiments.bash
+++ run-experiments.bash
@@ -4,41 +4,106 @@
 # author: Daniel Nichols
 # date: April 2022
 
+# ================
 # === SETTINGS ===
+# ================
 BUILD_EXEC="./bin/buildsa"
 QUERY_EXEC="./bin/querysa"
-BUILD_INPUT_SEQ="inputs/ecoli.fa"
+SEQUENCES="inputs/sars-cov-2.fa inputs/ecoli.fa inputs/fruitfly.fa"
 BUILD_SEQ_RESULTS_OUTPUT="results/buildsa-sequential.csv"
 BUILD_PAR_RESULTS_OUTPUT="results/buildsa-parallel.csv"
+QUERY_SEQ_RESULTS_OUTPUT="results/querysa-sequential.csv"
+QUERY_PAR_RESULTS_OUTPUT="results/querysa-parallel.csv"
 NUM_EXPERIMENT_REPITITIONS=5
 
+# =============
 # === TESTS ===
-# TODO
+# =============
+echo "Testing..."
+make clean > /dev/null && make test USE_OPENMP=0 > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Failed sequential tests!"
+    exit 1
+fi
+make clean > /dev/null && make test USE_OPENMP=1 > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Failed OpenMP tests!"
+    exit 1
+fi
+#ecoli test
+if [ -f inputs/ecoli.fa ]; then
+    make clean > /dev/null && make USE_OPENMP=1 > /dev/null
+    ${BUILD_EXEC} inputs/ecoli.fa tmp.sa > /dev/null
+    ${QUERY_EXEC} tmp.sa inputs/query.fa naive naive-output.txt > /dev/null
+    ${QUERY_EXEC} tmp.sa inputs/query.fa simpleaccel simpleaccel-output.txt > /dev/null
+    rm tmp.sa
 
+    sort -g -o naive-output.txt naive-output.txt    # match the expected output sorting
+    sort -g -o simpleaccel-output.txt simpleaccel-output.txt
+
+    diff naive-output.txt inputs/query_res.txt
+    if [ $? -ne 0 ]; then 
+        echo "Failed Naive query on ecoli data."
+        exit 1
+    fi
+
+    diff simpleaccel-output.txt inputs/query_res.txt
+    if [ $? -ne 0 ]; then
+        echo "Failed SimpleAccelerant query on ecoli data."
+        exit 1
+    fi
+
+    rm naive-output.txt #simpleaccel-output.txt
+fi
+
+# ===================
 # === EXPERIMENTS ===
+# ===================
+echo "Running experiments..."
 mkdir -p results
 
-# buildsa -- serial
-make clean && make USE_OPENMP=0
+# serial -- buildsa, querysa
+make clean > /dev/null && make USE_OPENMP=0 > /dev/null
 printf "seq_length,preftab,sa_build_time,preftab_build_time,file_size\n" > ${BUILD_SEQ_RESULTS_OUTPUT}
-for preftab_size in 0 1 2 4 8 16 32 64; do
-    output_file="junk-buildsa-output.sa"
-    for repition in {1..5}; do
-        ${BUILD_EXEC} ${BUILD_INPUT_SEQ} ${output_file} --preftab ${preftab_size} >> ${BUILD_SEQ_RESULTS_OUTPUT}
-        rm ${output_file}
+printf "seq_length,preftab,mode,num_queries,total_query_time,avg_query_time\n" > ${QUERY_SEQ_RESULTS_OUTPUT}
+for sequence in ${SEQUENCES}; do
+    for preftab_size in 10; do
+        sa_output_file="junk-buildsa-output.sa"
+        query_output_file="junk-querysa-output.txt"
+        query_input_file="${sequence%.fa}-queries.fa"
+        
+        for repetition in {1..5}; do
+            ${BUILD_EXEC} ${sequence} ${sa_output_file} --preftab ${preftab_size} >> ${BUILD_SEQ_RESULTS_OUTPUT}
+            ${QUERY_EXEC} ${sa_output_file} ${query_input_file} naive ${query_output_file} >> ${QUERY_SEQ_RESULTS_OUTPUT}
+            ${QUERY_EXEC} ${sa_output_file} ${query_input_file} simpleaccel ${query_output_file} >> ${QUERY_SEQ_RESULTS_OUTPUT}
+
+            rm -f ${sa_output_file} ${query_output_file}
+        done
     done
 done
 
-# buildsa -- parallel
-make clean && make USE_OPENMP=1
+# parallel -- buildsa, querysa
+make clean > /dev/null && make USE_OPENMP=1 > /dev/null
 printf "seq_length,preftab,sa_build_time,preftab_build_time,file_size\n" > ${BUILD_PAR_RESULTS_OUTPUT}
-for preftab_size in 0 1 2 4 8 16 32 64; do
-    output_file="junk-buildsa-output.sa"
-    for repition in {1..5}; do
-        ${BUILD_EXEC} ${BUILD_INPUT_SEQ} ${output_file} --preftab ${preftab_size} >> ${BUILD_PAR_RESULTS_OUTPUT}
-        rm ${output_file}
+printf "seq_length,preftab,mode,num_queries,total_query_time,avg_query_time\n" > ${QUERY_PAR_RESULTS_OUTPUT}
+for sequence in ${SEQUENCES}; do
+    for preftab_size in 10; do
+        sa_output_file="junk-buildsa-output.sa"
+        query_output_file="junk-querysa-output.txt"
+        query_input_file="${sequence%.fa}-queries.fa"
+        
+        for repetition in {1..5}; do
+            ${BUILD_EXEC} ${sequence} ${sa_output_file} --preftab ${preftab_size} >> ${BUILD_PAR_RESULTS_OUTPUT}
+            ${QUERY_EXEC} ${sa_output_file} ${query_input_file} naive ${query_output_file} >> ${QUERY_PAR_RESULTS_OUTPUT}
+            ${QUERY_EXEC} ${sa_output_file} ${query_input_file} simpleaccel ${query_output_file} >> ${QUERY_PAR_RESULTS_OUTPUT}
+
+            rm -f ${sa_output_file} ${query_output_file}
+        done
     done
 done
 
+# ====================
 # === PLOT RESULTS ===
+# ====================
+echo "Plotting results..."
 python3 generate-plots.py ${BUILD_SEQ_RESULTS_OUTPUT} ${BUILD_PAR_RESULTS_OUTPUT} junk
